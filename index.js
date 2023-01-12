6b20510f81f3b8dcc16aaaea245a8adce696be03
Naeem Hassan <naeemhassancomsats@gmail.com>
2023-01-12 23:46:53 +0500

    implemented main function for file reading, handled errors

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+const fs = require('fs')
+
 const main = (path) => {
-    
-};
+  if (fs.existsSync(path))
+    if (fs.lstatSync(path).isDirectory())
+      return Promise.reject({ message: 'Path is a directory' })
 
+  return new Promise((resolve, reject) => {
+    try {
+      const data = fs.readFileSync(path)
 
+      return resolve(JSON.parse(data))
+    } catch (err) {
+      if (err instanceof SyntaxError) {
+        return reject({ message: 'JSON Invalid' })
+      }
 
-module.exports = main;
+      return reject({ message: 'File Does Not Exist' })
+    }
+  })
+}
+
+module.exports = main
